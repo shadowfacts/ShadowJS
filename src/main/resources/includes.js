c90211756b68c6dda83ca721e079464cfc774cd7
Shadowfacts <me@shadowfacts.net>
2015-08-14 21:50:02 -0400

    Whoops

--- src/main/resources/includes.js
+++ src/main/resources/includes.js
@@ -1,3 +1,3 @@
-var Blocks = Java.type("net.shadowfacts.shadowscript.js.block.Blocks");
-var Items = Java.type("net.shadowfacts.shadowscript.js.item.Items");
-var ItemStacks = Java.type("net.shadowfacts.shadowscript.js.itemstack.ItemStacks");
+var Blocks = Java.type("net.shadowfacts.shadowjs.js.block.Blocks");
+var Items = Java.type("net.shadowfacts.shadowjs.js.item.Items");
+var ItemStacks = Java.type("net.shadowfacts.shadowjs.js.itemstack.ItemStacks");
