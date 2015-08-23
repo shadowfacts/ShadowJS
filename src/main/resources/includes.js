f79c9c065db48d8493996eb9a38e7ad25a2adee4
Shadowfacts <me@shadowfacts.net>
2015-08-23 18:30:23 -0400

    derp

--- src/main/resources/includes.js
+++ src/main/resources/includes.js
@@ -4,7 +4,6 @@ var ShadowJS = Java.type("net.shadowfacts.shadowjs.js.ShadowJSInterface");
 
 // Minecraft
 var Minecraft = Java.type("net.shadowfacts.shadowjs.js.MinecraftInterface");
-var ModLoader = Java.type("net.shadowfacts.shadowjs.js.ModLoader");
 var Blocks = Java.type("net.shadowfacts.shadowjs.js.block.Blocks");
 var Items = Java.type("net.shadowfacts.shadowjs.js.item.Items");
 var ItemStacks = Java.type("net.shadowfacts.shadowjs.js.itemstack.ItemStacks");
