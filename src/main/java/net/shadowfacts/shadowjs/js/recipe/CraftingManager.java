be22cb67eec19a93371100e83128e25d477a100c
Shadowfacts <me@shadowfacts.net>
2015-08-23 18:45:40 -0400

    Allow OreDict names in shapeless recipes
    Closes #4

--- src/main/java/net/shadowfacts/shadowjs/js/recipe/CraftingManager.java
+++ src/main/java/net/shadowfacts/shadowjs/js/recipe/CraftingManager.java
@@ -5,6 +5,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 import net.shadowfacts.shadowjs.api.item.Ingredient;
 
 import java.util.ArrayList;
@@ -107,23 +108,58 @@ public class CraftingManager {
 		GameRegistry.addRecipe(new ShapedOreRecipe(result.getItemStack(), params.toArray()));
 	}
 
-	public static void registerShapelessRecipe(Ingredient result, Ingredient... inputs) {
+	public static void registerShapelessRecipe(Ingredient result, Object... inputs) {
 		if (inputs.length > 9) throw new RuntimeException("Shapeless recipe cannot have more than 9 inputs");
 		if (inputs[0] == null) throw new RuntimeException("Shapeless recipe must have at least 1 input");
 
-		ArrayList<ItemStack> params = new ArrayList<>();
+		ArrayList<Object> params = new ArrayList<>();
 
-		params.add(inputs[0].getItemStack());
-		if (inputs.length > 1 && inputs[1] != null) params.add(inputs[1].getItemStack());
-		if (inputs.length > 2 && inputs[2] != null) params.add(inputs[2].getItemStack());
-		if (inputs.length > 3 && inputs[3] != null) params.add(inputs[3].getItemStack());
-		if (inputs.length > 4 && inputs[4] != null) params.add(inputs[4].getItemStack());
-		if (inputs.length > 5 && inputs[5] != null) params.add(inputs[5].getItemStack());
-		if (inputs.length > 6 && inputs[6] != null) params.add(inputs[6].getItemStack());
-		if (inputs.length > 7 && inputs[7] != null) params.add(inputs[7].getItemStack());
-		if (inputs.length > 8 && inputs[8] != null) params.add(inputs[8].getItemStack());
+		if (inputs[0] instanceof Ingredient)  params.add(((Ingredient)inputs[0]).getItemStack());
+		else if (inputs[0] instanceof String) params.add(inputs[0]);
+		else throw new RuntimeException("The first ingredient parameter should be an Ingredient or a string");
 
-		GameRegistry.addShapelessRecipe(result.getItemStack(), params.toArray());
+		if (inputs.length > 1 && inputs[1] != null) {
+			if (inputs[1] instanceof Ingredient)  params.add(((Ingredient)inputs[1]).getItemStack());
+			else if (inputs[1] instanceof String) params.add(inputs[1]);
+			else throw new RuntimeException("The second ingredient parameter should be an Ingredient or a string");
+		}
+		if (inputs.length > 2 && inputs[2] != null) {
+			if (inputs[2] instanceof Ingredient)  params.add(((Ingredient)inputs[2]).getItemStack());
+			else if (inputs[2] instanceof String) params.add(inputs[2]);
+			else throw new RuntimeException("The third ingredient parameter should be an Ingredient or a string");
+		}
+		if (inputs.length > 3 && inputs[3] != null) {
+			if (inputs[3] instanceof Ingredient)  params.add(((Ingredient)inputs[3]).getItemStack());
+			else if (inputs[3] instanceof String) params.add(inputs[3]);
+			else throw new RuntimeException("The fourth ingredient parameter should be an Ingredient or a string");
+		}
+		if (inputs.length > 4 && inputs[4] != null) {
+			if (inputs[4] instanceof Ingredient)  params.add(((Ingredient)inputs[4]).getItemStack());
+			else if (inputs[4] instanceof String) params.add(inputs[4]);
+			else throw new RuntimeException("The fifth ingredient parameter should be an Ingredient or a string");
+		}
+		if (inputs.length > 5 && inputs[5] != null) {
+			if (inputs[5] instanceof Ingredient)  params.add(((Ingredient)inputs[5]).getItemStack());
+			else if (inputs[5] instanceof String) params.add(inputs[5]);
+			else throw new RuntimeException("The sixth ingredient parameter should be an Ingredient or a string");
+		}
+		if (inputs.length > 6 && inputs[6] != null) {
+			if (inputs[0] instanceof Ingredient)  params.add(((Ingredient)inputs[0]).getItemStack());
+			else if (inputs[0] instanceof String) params.add(inputs[0]);
+			else throw new RuntimeException("The first ingredient parameter should be an Ingredient or a string");
+		}
+		if (inputs.length > 7 && inputs[7] != null) {
+			if (inputs[7] instanceof Ingredient)  params.add(((Ingredient)inputs[7]).getItemStack());
+			else if (inputs[7] instanceof String) params.add(inputs[7]);
+			else throw new RuntimeException("The eighth ingredient parameter should be an Ingredient or a string");
+		}
+		if (inputs.length > 8 && inputs[8] != null) {
+			if (inputs[9] instanceof Ingredient)  params.add(((Ingredient)inputs[9]).getItemStack());
+			else if (inputs[9] instanceof String) params.add(inputs[9]);
+			else throw new RuntimeException("The ninth ingredient parameter should be an Ingredient or a string");
+		}
+
+		GameRegistry.addRecipe(new ShapelessOreRecipe(result.getItemStack(), params.toArray()));
 	}
 
 	@SuppressWarnings("unchecked")
