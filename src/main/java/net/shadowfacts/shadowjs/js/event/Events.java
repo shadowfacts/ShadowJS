22020f4a04d2ff4f6bf4306e121dedd0cea648a5
Shadowfacts <me@shadowfacts.net>
2015-08-24 21:46:34 -0400

    Add to Events

--- src/main/java/net/shadowfacts/shadowjs/js/event/Events.java
+++ src/main/java/net/shadowfacts/shadowjs/js/event/Events.java
@@ -1,11 +1,13 @@
 package net.shadowfacts.shadowjs.js.event;
 
-import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.event.ServerChatEvent;
-import net.minecraftforge.event.brewing.PotionBrewEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.shadowfacts.shadowjs.js.event.wrapper.BlockEvents;
+import net.shadowfacts.shadowjs.js.event.wrapper.ExplosionEvents;
+import net.shadowfacts.shadowjs.js.event.wrapper.ItemEvents;
+import net.shadowfacts.shadowjs.js.event.wrapper.ServerChatMessageReceived;
 
 /**
  * @author shadowfacts
@@ -14,13 +16,11 @@ public enum Events {
 
 	HarvestBlockDrops(BlockEvent.HarvestDropsEvent.class, BlockEvents.HarvestDrops.class),
 	BreakBlock(BlockEvent.BreakEvent.class, BlockEvents.Break.class),
-	PlaceBlock(BlockEvent.PlaceEvent.class, BlockEvents.Place.class);
-//	ServerChatReceived(ServerChatEvent.class),
-//	CommandExecuted(CommandEvent.class),
-//	PrePotionBrew(PotionBrewEvent.Pre.class),
-//	PostPotionBrew(PotionBrewEvent.Post.class),
-//	ExplosionStart(ExplosionEvent.Start.class),
-//	ExplosionDetonate(ExplosionEvent.Detonate.class);
+	PlaceBlock(BlockEvent.PlaceEvent.class, BlockEvents.Place.class),
+	ExplosionStart(ExplosionEvent.Start.class, ExplosionEvents.Start.class),
+	ExplosionDetonate(ExplosionEvent.Detonate.class, ExplosionEvents.Detonate.class),
+	ServerChatMessageReceived(ServerChatEvent.class, ServerChatMessageReceived.class),
+	ItemThrown(ItemTossEvent.class, ItemEvents.ItemThrownEvent.class);
 
 
 	Class forgeEventClass;
